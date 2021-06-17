146f7e0db59f0b536c16379aae796d00b2cdbe5f
Alexej <kovernikov@post.cz>
2021-06-17 23:11:04 +0200

    Updates

---
+++ precache-manifest.004ad3b09845c1f0b37065a0e42c8c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c16115238bccdc4ca33667c4fcfccea",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "984b31ce85a142f304ed",
+    "url": "/TodoList/static/css/main.c050a4b5.chunk.css"
+  },
+  {
+    "revision": "0049ed3fa72de995a10e",
+    "url": "/TodoList/static/js/2.11c75dd7.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.11c75dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984b31ce85a142f304ed",
+    "url": "/TodoList/static/js/main.236acfb8.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
